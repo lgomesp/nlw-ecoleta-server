80733b11e1f1fc41df30caef4052c5d6c976e9a1
Lucas Gomes <lucasgomesp22@gmail.com>
2020-06-02 16:41:55 -0300

    add dependecies and set a basic server

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('listagem de usuários')
+
+  response.json(['Diego', 'Cleiton', 'Robson', 'Lucas'])
+});
+
+app.listen(3333);
