eeb1b3929195777d518803602d144f5ff897139d
Lucas Gomes <lucasgomesp22@gmail.com>
2020-06-03 00:22:12 -0300

    clean up files and create separate routes file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
+import express from 'express'
+import routes from './routes'
 
-const app = express();
+const app = express()
 
-app.get('/users', (request, response) => {
-  console.log('listagem de usuários')
+app.use(express.json())
 
-  response.json(['Diego', 'Cleiton', 'Robson', 'Lucas'])
-});
+app.use(routes)
 
-app.listen(3333);
+app.listen(3333)
