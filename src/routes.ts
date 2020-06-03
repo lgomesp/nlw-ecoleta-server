3ebe0ae11053c43e28720bdadcb2d5248df6ce70
Lucas Gomes <lucasgomesp22@gmail.com>
2020-06-03 19:56:40 -0300

    create all app api

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
 const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/', (request, response) => {
+routes.get('/items', itemsController.index)
 
-  return response.json({ message: "hello world" })
-  
-})
+routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
 export default routes
